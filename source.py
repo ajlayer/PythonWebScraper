a063417b07a2376e5b69e9edd37476342d604d19
AJ Loehr <ajloehr7@gmail.com>
2018-03-10 14:11:40 -0500

    added SnagAJob.com scraping method

--- source.py
+++ source.py
@@ -1,7 +1,6 @@
 # Scrapes various job websites & stores listings in a database
 
-from urllib.request import urlopen
-from urllib.error import HTTPError
+from urllib import urlopen
 from bs4 import BeautifulSoup
 import sqlite3
 import sys
@@ -15,6 +14,7 @@ snagajob = "https://www.snagajob.com/job-search?ui=true&q=warehouse&w=17401"
 
 # global job id counter
 x = 0
+
 # Connect to database
 print("Connecting to database...")
 try:
@@ -26,22 +26,29 @@ except sqlite3.Error as e:
     print(e)
     sys.exit()
 
-# TODO: Add arrays to DB
+
+# Gets jobs from monster.com and adds the information to a database
 def getJobsFromMonster(url):
+    # Try to connect to website URL
     try:
         html = urlopen(url)
-    except HTTPError as e:
+    # If it can not connect
+    except AttributeError as e:
         print("Error:")
         print(e)
         return None
+    # Try to parse the web page into a beautifulsoup object
     try:
         bsObj = BeautifulSoup(html, "html.parser")
+    # If it cant create a beautifulsoup object
     except AttributeError as e:
         print("Error:")
         print(e)
         return None
 
     # Scrape for specific tags
+
+    # TODO: Get web id from database of websites
     web_id = 1
     titleList = bsObj.findAll("div", {"class":"jobTitle"})
     companyList = bsObj.findAll("div", {"class":"company"})
@@ -72,35 +79,79 @@ def getJobsFromMonster(url):
         # Adds date to date list
         dates.append(name.get_text().strip())
 
-    # TODO: add arrays to DB
-    for x in range(0, len(titles)):
-        tuple = [(x, web_id, titles[x], locations[x], companies[x], dates[x])]
-        print(tuple)
-        # Add tuple to database
-        c.executemany('INSERT INTO jobs VALUES(?,?,?,?,?,?)',tuple)
 
-# TODO: Fix formatting
-# TODO: Store info in variables
-# TODO: Add info to DB
+    for x in range(0, len(titles)):
+        tupe = [(x, web_id, titles[x], locations[x], companies[x], dates[x])]
+        print(tupe)
+        # Add tuple to database
+        #c.executemany('INSERT INTO jobs VALUES(?,?,?,?,?,?)', tupe)
+
+# TODO: Location
+# Gets jobs from snagajob.com and adds the information to a database
 def getJobsFromSnagajob(url):
+    web_id = 2
+    # Try to connect to website URL
     try:
         html = urlopen(url)
+    # If it can not connect
     except AttributeError as e:
         print("Error:")
         print(e)
         return None
+    # Try to parse the web page into a beautifulsoup object
     try:
         bsObj = BeautifulSoup(html, "html.parser")
+    # If it cant create a beautifulsoup object
     except AttributeError as e:
         print("Error:")
         print(e)
         return None
-    # TODO: Scrape for specifics
-    tag = "results"
-    nameList = bsObj.findAll("div", {"class":tag})
-    for name in nameList:
-        # TODO: add to database instead of print
-        print(name.get_text())
+
+    web_id = 2
+    # <h2 class="result-title">
+    titleList2 = bsObj.findAll("h2", {"class": "result-title"})
+    # Company name is stored in <div> </div> tags
+    # TODO: figure out how to scrape for companies
+    #companyList2 = bsObj.findAll("div", {"class": "company"})
+    # <span class="location text-muted">
+    locationList2 = bsObj.findAll("span", {"class": "location text-muted"})
+    # <small class = text-success updated">
+    dateList2 = bsObj.findAll("small", {"class": "text-success updated"})
+    titles2 = []
+    companies2 = []
+    locations2 = []
+    dates2 = []
+
+    # Titles
+    for name in titleList2:
+        # Adds title to titles list
+        titles2.append(name.get_text().strip())
+
+    # Companies
+    #for name in companyList:
+        # Adds company name to companies list
+    #    companies.append(name.get_text().strip())
+
+    # Locations
+    for name in locationList2:
+        # Adds location to location list
+        locations2.append(name.get_text().strip())
+
+    # Dates
+    for name in dateList2:
+        # Adds date to date list
+        dates2.append(name.get_text().strip())
+    x = 0
+    # Used to figure out limiting factor for the for loop
+    #print(len(titles2))
+    #print(len(locations2))
+    #print(len(dates2))
+
+    for x in range(0, 14):
+        tupe = [(x, web_id, titles2[x], locations2[x], 'Coming Soon!', dates2[x])]
+        print(tupe)
+        c.executemany('INSERT INTO jobs VALUES(?, ?, ?, ?, ?, ?)', tupe)
+
 
 # TODO: Fix formatting
 # TODO: Store info in variables
@@ -125,6 +176,7 @@ def getJobsFromIndeed(url):
         # TODO: add to database instead of print
         print(name.get_text())
 
+
 # TODO: Fix formatting
 # TODO: Store info in variables
 # TODO: Add info to DB
@@ -148,19 +200,27 @@ def getJobsFromZipRecruiter(url):
         # TODO: add to database instead of print
         print(name.get_text())
 
-# Prints results
-print("**********Monster**********")
-getJobsFromMonster(monster)
-print("**********SnagAJob**********")
+
+# Gets results
+
+#print("**********Monster**********")
+# Every time this gets run it adds all the data to the database,
+# No point in running it more than once because it just adds the same
+# Data set every time
+#getJobsFromMonster(monster)
+
+#print("**********SnagAJob**********")
 #getJobsFromSnagajob(snagajob)
-print("**********Indeed**********")
+
+#print("**********Indeed**********")
 #getJobsFromIndeed(indeed)
-print("**********ZipRecruiter**********")
+#print("**********ZipRecruiter**********")
 #getJobsFromZipRecruiter(ziprecruiter)
 
 # Print database contents
-for row in c.execute('SELECT * FROM jobs ORDER BY job_number'):
+for row in c.execute('SELECT * FROM jobs ORDER BY web_id'):
     print(row)
+
 
 # Commit changes to database
 conn.commit()
